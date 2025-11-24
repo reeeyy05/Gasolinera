9a33e48c6a7d93ada26bb7949efe87027c51cbfe
alejandro_rey <areyt01@iesalbarregas.es>
2025-11-24 13:59:46 +0100

    🔁Update: actualizacion de los archivos
    Se actualiza la sintasis de los archivos

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+import { base } from './constants.js';
+
+const selectProvincia = document.getElementById("select-provincia");
+const selectMunicipio = document.getElementById("select-municipio");
+const selectCombustible = document.getElementById("select-combustible");
+const checkAbiertas = document.getElementById("check-abiertas");
+const botonBuscar = document.getElementById("boton-buscar");
+const resultados = document.getElementById("resultados");
