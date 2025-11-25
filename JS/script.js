8dae0bdf940b8a371c515a9e2732bd57f63329a3
alejandro_rey <areyt01@iesalbarregas.es>
2025-11-25 13:43:55 +0100

    ✅New: funcion combustibles
    Se agrega la funcion para mostrar los combustibles y un css

--- JS/script.js
+++ JS/script.js
@@ -7,12 +7,19 @@ const checkAbiertas = document.getElementById('check-abiertas');
 const botonBuscar = document.getElementById('boton-buscar');
 const resultados = document.getElementById('resultados');
 
+document.addEventListener("DOMContentLoaded", async () => {
+    cargarProvincias();
+    cargarCombustibles();
+});
+
+// Rellenamos el select de provincias
 async function cargarProvincias() {
     try {
         const respuesta = await fetch(`${base}/Listados/Provincias`);
         const data = await respuesta.json();
         selectProvincia.innerHTML = '<option value="">Seleccione una provincia</option>';
 
+        // Lo rellenamos con cada provincia recibida
         data.forEach(prov => {
             selectProvincia.innerHTML += `<option value="${prov.IDPovincia}">${prov.Provincia}</option>`;
         });
@@ -21,16 +28,19 @@ async function cargarProvincias() {
     }
 }
 
+// Al cambiar la provincia se actualiza los municipios
 selectProvincia.addEventListener("change", async () => {
     const idProv = selectProvincia.value;
     selectMunicipio.disabled = !idProv;
     selectMunicipio.innerHTML = '<option value="">Seleccione un municipio</option>';
 
+    // Si no hay provincia se sale de la funcion
     if (!idProv) return;
 
     try {
         const respuesta = await fetch(`${base}/Listados/MunicipiosPorProvincia/${idProv}`);
         const data = await respuesta.json();
+        // Recorremos los municipios obtenidos
         data.forEach(mun => {
             selectMunicipio.innerHTML += `<option value="${mun.IDMunicipio}">${mun.Municipio}</option>`;
         });
@@ -38,3 +48,18 @@ selectProvincia.addEventListener("change", async () => {
         selectMunicipio.innerHTML += `<option value="">Error al cargar municipios</option>`;
     }
 });
+
+async function cargarCombustibles() {
+    try {
+        // Pedimos el listado de combustibles a la API
+        const res = await fetch(`${base}/Listados/ProductosPetroliferos`);
+        const data = await res.json();
+        selectCombustible.innerHTML = '<option value="">Seleccione combustible</option>';
+        // Añadimos cada combustible recibido como opción del select
+        data.forEach(prod => {
+            selectCombustible.innerHTML += `<option value="${prod.IDProducto}">${prod.NombreProducto}</option>`;
+        });
+    } catch (e) {
+        selectCombustible.innerHTML = "<option>Error al cargar combustibles</option>";
+    }
+}
